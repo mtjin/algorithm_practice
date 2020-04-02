e97405d4415f0fc83aa3a04454eaca7492dd5105
jackjackE <tmddjs210@naver.com>
2020-04-03 00:10:40 +0900

    백준 1149 RGB 거리 풀이

--- 백준 1149 RGB거리/Main.java
+++ 백준 1149 RGB거리/Main.java
@@ -5,19 +5,21 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] house = new int[N + 1][3]; //2차원배열 RGB
+        int[] R = new int[N + 1]; //2차원배열 RGB
+        int[] G = new int[N + 1];
+        int[] B = new int[N + 1];
 
         for (int i = 1; i <= N; i++) {
-            house[i][0] = sc.nextInt(); //R
-            house[i][1] = sc.nextInt(); //R
-            house[i][2] = sc.nextInt(); //R
+            R[i] = sc.nextInt(); //R
+            G[i] = sc.nextInt(); //G
+            B[i] = sc.nextInt(); //B
         }
         for (int i = 1; i < N; i++) {
-            house[i + 1][0] += Math.min(house[i][1], house[i][2]);
-            house[i + 1][1] += Math.min(house[i][0], house[i][2]);
-            house[i + 1][2] += Math.min(house[i][0], house[i][1]);
+            R[i + 1] += Math.min(G[i], B[i]);
+            G[i + 1] += Math.min(R[i], B[i]);
+            B[i + 1] += Math.min(R[i], G[i]);
         }
-        System.out.println(Math.min(Math.min(house[N][0], house[N][1]), house[N][2]));
+        System.out.println(Math.min(Math.min(R[N], G[N]), B[N]));
     }
 
 
