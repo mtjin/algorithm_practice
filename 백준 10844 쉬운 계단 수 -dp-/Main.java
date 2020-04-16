7aa88d4e71f4c6eba3b6a2fb029b4dbcea8260c0
jackjackE <tmddjs210@naver.com>
2020-04-16 16:20:04 +0900

    백준 10844 쉬운 계단 수 풀이

---
+++ 백준 10844 쉬운 계단 수 -dp-/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[][] dp = new long[N + 1][10];
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 9) {
+                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                }
+            }
+        }
+        long answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += dp[N][i];
+        }
+        System.out.println(answer % 1000000000);
+    }
+}
