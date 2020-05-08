78e1a26ae27302ed9a1f0dfd035d6c976c0a138c
jackjackE <tmddjs210@naver.com>
2020-05-08 16:58:32 +0900

    알고리즘 복습하며 코드 수정

--- 프로그래머스 다리를 지나는 트럭/Solution.kts
+++ 프로그래머스 다리를 지나는 트럭/Solution.kts
@@ -3,8 +3,8 @@ import java.util.*
 class Solution {
 
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        val readyQueue: Queue<Truck> = LinkedList<Truck>()  //출발전 대기큐
-        val onBridgeQueue: Queue<Truck> = LinkedList<Truck>() //다리위 출발큐
+        val readyQueue: Queue<Truck> = LinkedList<Truck>()  //트럭 대기큐
+        val onBridgeQueue: Queue<Truck> = LinkedList<Truck>() //다리위 트럭 출발큐
         var onBridgeTotalTruckWeight = 0 //다리 위 현재 무게
         var time = 0
 
@@ -13,15 +13,17 @@ class Solution {
             readyQueue.offer(Truck(weight, 0))
         }
 
-        //미리 출발큐에 하나 추가
-        if(readyQueue.size >= 0) {
-            onBridgeTotalTruckWeight += readyQueue.peek().weight
-            onBridgeQueue.offer(readyQueue.poll())
-        }else{
-            return 0
-        }
-
-        while (!onBridgeQueue.isEmpty()) {
+        while (true) {
+            //대기큐에 트럭이 있을 경우
+            if (readyQueue.isNotEmpty()) {
+                // 다리가 무게 견딜수 있는 트럭인 경우 트럭 poll(출발)
+                if (readyQueue.peek().weight + onBridgeTotalTruckWeight <= weight) {
+                    with(readyQueue.poll()) {
+                        onBridgeTotalTruckWeight += this.weight
+                        onBridgeQueue.offer(this)
+                    }
+                }
+            }
             //다리 위 트럭이동
             for (truck2: Truck in onBridgeQueue) {
                 truck2.postion++
@@ -30,21 +32,14 @@ class Solution {
             if (onBridgeQueue.peek().postion >= bridge_length) {
                 onBridgeTotalTruckWeight -= onBridgeQueue.poll().weight
             }
-            //대기큐에 트럭이 있을 경우
-            if (!readyQueue.isEmpty()) {
-                // 다리가 무게 견딜수 있는 트럭인 경우 트럭 poll(출발)
-                if (readyQueue.peek().weight + onBridgeTotalTruckWeight <= weight) {
-                    with(readyQueue.poll()){
-                        onBridgeTotalTruckWeight += this.weight
-                        onBridgeQueue.offer(this)
-                    }
-                }
-            }
             //총 걸린 시간 증가(결과)
             time++
+            // 다 끝난 경우
+            if(readyQueue.isEmpty() && onBridgeQueue.isEmpty()){
+                break
+            }
         }
-
-        return time + 1 //마지막 빠져나오는 시간떄문에 +1
+        return time + 1//마지막 빠져나오는 시간떄문에 +1
     }
 
     //트럭무게, 현재위치
