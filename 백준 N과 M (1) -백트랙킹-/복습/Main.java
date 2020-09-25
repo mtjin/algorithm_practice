d99a829b82047198797f674157a9ef0cccca50f4
jackjackE <tmddjs210@naver.com>
2020-09-26 03:31:34 +0900

    백준 N과 M 복습

---
+++ 백준 N과 M (1) -백트랙킹-/복습/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static boolean[] isVisited;
+    static int[] num;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        isVisited = new boolean[N + 1];
+        num = new int[N + 1];
+        dfs(1);
+
+    }
+
+    static void dfs(int current) {
+        if (current > M) {
+            for (int i = 1; i <= M; i++) {
+                System.out.print(num[i] + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 1; i <= N; i++) {
+                if (isVisited[i]) {
+                    continue;
+                }
+                isVisited[i] = true;
+                num[current] = i;
+                dfs(current + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+
+
+}
