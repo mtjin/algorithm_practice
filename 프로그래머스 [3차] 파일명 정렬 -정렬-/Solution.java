8a4aedbc67d2941de63e6f58e8a751381dedde40
jackjackE <tmddjs210@naver.com>
2020-10-12 14:18:25 +0900

    프로그래머스 파일명 정렬 풀이

--- 프로그래머스 [3차] 파일명 정렬 -정렬-/Solution.java
+++ 프로그래머스 [3차] 파일명 정렬 -정렬-/Solution.java
@@ -8,30 +8,28 @@ class Solution {
 
     public String[] solution(String[] files) {
         Arrays.sort(files, (o1, o2) -> {
-            String head1 = o1.split("[0-9]")[0].toLowerCase();
+            String head1 = o1.split("[0-9]")[0].toLowerCase(); // head - number, tail 분리
             String head2 = o2.split("[0-9]")[0].toLowerCase();
-            if (head1.compareTo(head2) == 0) {
-                String numTail1 = o1.substring(head1.length());
+            if (head1.compareTo(head2) == 0) { // head 같은문자열인 경우
+                String numTail1 = o1.substring(head1.length()); // number,tail 남김
                 String numTail2 = o2.substring(head2.length());
-                StringBuilder sb1 = new StringBuilder();
-                for (char c : numTail1.toCharArray()) {
-                    if (Character.isDigit(c) && sb1.length() <= 5)
-                        sb1.append(c);
-                    else
-                        break;
-                }
-                StringBuilder sb2 = new StringBuilder();
-                for (char c : numTail2.toCharArray()) {
-                    if (Character.isDigit(c) && sb2.length() <= 5)
-                        sb2.append(c);
-                    else
-                        break;
-                }
-                return Integer.parseInt(sb1.toString()) - Integer.parseInt(sb2.toString());
+                return Integer.parseInt(calNumber(numTail1)) - Integer.parseInt(calNumber(numTail2));
             } else {
                 return head1.compareTo(head2);
             }
         });
         return files;
+    }
+
+    private static String calNumber(String numTail2) { //최대 5자리인 number 계산
+        StringBuilder sb2 = new StringBuilder();
+        for (char c : numTail2.toCharArray()) {
+            if (Character.isDigit(c) && sb2.length() <= 5) {
+                sb2.append(c);
+            } else {
+                return sb2.toString();
+            }
+        }
+        return sb2.toString();
     }
 }
