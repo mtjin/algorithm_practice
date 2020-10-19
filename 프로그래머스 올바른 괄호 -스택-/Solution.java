d02a6cd346428eec4247fda495e9d1a074cec3f8
jackjackE <tmddjs210@naver.com>
2020-10-19 22:50:49 +0900

    프로그래머스 올바른 괄호 풀이

---
+++ 프로그래머스 올바른 괄호 -스택-/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
