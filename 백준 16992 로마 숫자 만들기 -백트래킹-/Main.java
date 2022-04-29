305eab7698ef3ecc22adcd4bbe8b4c291ab703f4
jackjackE <tmddjs210@naver.com>
2022-04-30 00:18:12 +0900

    백준 16992 풀이

--- 백준 16992 로마 숫자 만들기 -백트래킹-/Main.java
+++ 백준 16992 로마 숫자 만들기 -백트래킹-/Main.java
@@ -1,26 +1,30 @@
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     private static int N; // 로마 숫자 N개
     private static int[] nums = new int[]{1, 5, 10, 50};
-    private static HashSet<Integer> set = new HashSet<>();
+    private static boolean[] isVisited = new boolean[1001];
+    private static int result = 0;
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        dfs(0, 0);
-        System.out.println(set.size());
+        dfs(0, 0, 0);
+        System.out.println(result);
     }
 
-    private static void dfs(int depth, int sum) {
+    private static void dfs(int depth, int index, int sum) {
         if (depth == N) {
-            set.add(sum);
+            if (!isVisited[sum]) {
+                isVisited[sum] = true;
+                result++;
+            }
             return;
         }
-        for (int i = 0; i < 4; i++) {
-            dfs(depth + 1, sum + nums[i]);
+        for (int i = index; i < 4; i++) {
+            // 중복탐색안되게끔 index 설정
+            dfs(depth + 1, i, sum + nums[i]);
         }
     }
 }
